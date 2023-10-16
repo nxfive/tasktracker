11290686235ce19c238bd919c61440f29e84e4fe
nxfive <nxfivecom@gmail.com>
2023-10-16 23:11:45 +0200

    feat: Add methods for handling user update and check passwords
    The `as_form` method simplify creating instances from form data,
    and the `check_passwords` method validates password updates.

--- schemas/user.py
+++ schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, root_validator
 from fastapi import Form
 
 
@@ -30,9 +30,36 @@ class UserCreate(BaseModel):
             confirm_password=confirm_password
         )
 
+    @classmethod
+    @root_validator
+    def verify_password_match(cls, values):
+        password = values.get("password")
+        confirm_password = values.get("confirm_password")
+
+        if password != confirm_password:
+            raise ValueError("Passwords did not match")
+        return values
+
 
 class UserUpdate(BaseModel):
     email: EmailStr | None = None
     old_password: str | None = None
     new_password: str | None = None
 
+    @classmethod
+    def as_form(cls,
+                email: str = Form(),
+                old_password: str = Form(),
+                new_password: str = Form()):
+        return cls(
+            email=email,
+            old_password=old_password,
+            new_password=new_password
+        )
+
+    @classmethod
+    @root_validator(pre=True)
+    def check_passwords(cls, values):
+        if "new_password" in values and "old_password" not in values:
+            raise ValueError("Old password must be provided!")
+        return values
