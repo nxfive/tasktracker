1c01f6c2ea2b9791ff2e2e46ccbc1bd5c4e160af
nxfive <nxfivecom@gmail.com>
2023-10-19 22:30:28 +0200

    feat: Add task related API routes and handlers

---
+++ routers/task.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, status
+from typing import List, Annotated
+from schemas.task import TaskDisplay, TaskUpdate, TaskCreate
+from schemas.user import UserBase
+from sqlalchemy.orm import Session
+from database.setup import get_db
+from crud.task import crud_task
+from auth.oauth2 import get_current_user
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["tasks"],
+)
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=List[TaskDisplay])
+def get_all_tasks(db: Annotated[Session, Depends(get_db)], current_user: [UserBase, Depends(get_current_user)]):
+    return crud_task.get_all_tasks(db, user_id=current_user.id)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_task(request: TaskCreate,
+                db: Annotated[Session, Depends(get_db)],
+                current_user: [UserBase, get_current_user]
+                ):
+    return crud_task.create_task(db, request, user_id=current_user.id)
+
+
+@router.patch("/update/{task_id}", status_code=status.HTTP_200_OK)
+def update_task(task_id: int, request: TaskUpdate,
+                db: Annotated[Session, Depends(get_db)],
+                current_user: [UserBase, get_current_user]
+                ):
+    return crud_task.update_task(db, request, task_id=task_id, user_id=current_user.id)
+
+
+@router.delete("/delete/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int,
+                db: Annotated[Session, Depends(get_db)],
+                current_user: [UserBase, get_current_user]
+                ):
+    return crud_task.delete_task(db, task_id=task_id, user_id=current_user.id)
