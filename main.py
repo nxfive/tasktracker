969f484654ec4720e5c0978689c2ce4503f6a299
nxfive <nxfivecom@gmail.com>
2023-10-22 23:36:31 +0200

    feat: Add to app router group and task router

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from database.setup import Base, engine
 from fastapi import FastAPI
-from routers import user
+from routers import user, group, task
 from auth import authentication
 
 app = FastAPI()
 
-app.include_router(user.router)
 app.include_router(authentication.router)
+app.include_router(user.router)
+app.include_router(task.router)
+app.include_router(group.router)
 
 Base.metadata.create_all(engine)
 
