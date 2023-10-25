582ae77b44296e00189b6a750738e2e6b1dbdde2
nxfive <nxfivecom@gmail.com>
2023-10-25 23:03:07 +0200

    feat: Add to app router label, board and admin routers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from database.setup import Base, engine
 from fastapi import FastAPI
-from routers import user, group, task
+from routers import user, group, task, label, board, admin
 from auth import authentication
 
 app = FastAPI()
@@ -9,6 +9,9 @@ app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(task.router)
 app.include_router(group.router)
+app.include_router(label.router)
+app.include_router(board.router)
+app.include_router(admin.router)
 
 Base.metadata.create_all(engine)
 
