5c127bfefef17f6bc4796d01c9977011e1c02b09
nxfive <nxfivecom@gmail.com>
2023-10-26 22:06:57 +0200

    feat: Create enum class Role and update validation using Validate

--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel, EmailStr, root_validator
 from fastapi import Form
+from enum import Enum
+from schemas.tools import Validate
+
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
 
 
 class UserBase(BaseModel):
@@ -33,11 +40,7 @@ class UserCreate(BaseModel):
     @classmethod
     @root_validator
     def verify_password_match(cls, values):
-        password = values.get("password")
-        confirm_password = values.get("confirm_password")
-
-        if password != confirm_password:
-            raise ValueError("Passwords did not match")
+        Validate().validate_passwords_match(new=values.get("password", 0), confirm=values.get("confirm_password", 0))
         return values
 
 
@@ -45,23 +48,29 @@ class UserUpdate(BaseModel):
     email: EmailStr | None = None
     old_password: str | None = None
     new_password: str | None = None
+    confirm_password: str | None = None
 
     @classmethod
     def as_form(cls,
                 email: str = Form(),
                 old_password: str = Form(),
-                new_password: str = Form()):
+                new_password: str = Form(),
+                confirm_password: str = Form()):
         return cls(
             email=email,
             old_password=old_password,
-            new_password=new_password
+            new_password=new_password,
+            confirm_password=confirm_password
         )
 
     @classmethod
     @root_validator(pre=True)
     def check_passwords(cls, values):
-        if "new_password" in values and "old_password" not in values:
-            raise ValueError("Old password must be provided!")
+        Validate.validate_password_change(
+            old=values.get("old_password", 0),
+            new=values.get("new_password", 0),
+            confirm=values.get("confirm_password", 0)
+        )
         return values
 
 
