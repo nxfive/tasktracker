6d66af128930f33d5082c24879da57d43abab495
nxfive <nxfivecom@gmail.com>
2023-10-29 23:13:38 +0100

    feat: Add typing for return values

--- database/hash.py
+++ database/hash.py
@@ -4,11 +4,10 @@ pwd_ctx = CryptContext(schemes="bcrypt", deprecated="auto")
 
 
 class Hash:
-
     @staticmethod
-    def bcrypt(password: str):
+    def bcrypt(password: str) -> str:
         return pwd_ctx.hash(password)
 
     @staticmethod
-    def verify(plain_password, hashed_password):
+    def verify(plain_password: str, hashed_password: str) -> bool:
         return pwd_ctx.verify(secret=plain_password, hash=hashed_password)
