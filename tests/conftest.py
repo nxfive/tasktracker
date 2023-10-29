fc087374ab9594af22d394b0716be74f0c66f7ea
nxfive <nxfivecom@gmail.com>
2023-10-29 23:20:55 +0100

    feat: Refactor fixtures for test database setup
    It adds fixture for applictation setup, change fixture session to
    use get_engine and get_session functions from database/setup.py.
    Creates  fixture for groups creation one by authenticated user
    and one by non-authenticated.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,35 @@
+from os import environ
 import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from main import app
-from database.setup import get_db, Base
 from auth.oauth2 import create_access_token
 from crud.task import crud_task
+from crud.group import crud_group
 from schemas.task import TaskCreate
+from schemas.group import GroupCreate
 from models.task import DbTask
+from models.group import DbGroup
 from typing import List
+from sqlalchemy.orm import Session
+from schemas.user import UserBase
+from typing import Generator, Any
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@127.0.0.1:5432/test"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
-
-TestSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
-)
-
-
-def override_get_db():
-    db = TestSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+environ["env_state"] = "test"
+from database.setup import get_db, Base, get_engine, get_session  # noqa: E402
+from main import get_app  # noqa: E402
 
 
 @pytest.fixture()
-def session():
+def app() -> FastAPI:
+    return get_app()
+
+
+@pytest.fixture()
+def session() -> Generator[Session]:
+    engine = get_engine()
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    db = TestSessionLocal()
+    db = get_session()
     try:
         yield db
     finally:
@@ -39,25 +37,36 @@ def session():
 
 
 @pytest.fixture()
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
+def client(app, session) -> Generator[TestClient]:
     yield TestClient(app)
 
 
 @pytest.fixture()
-def test_user(client):
+def test_user(client: TestClient):
     data = {
         "name": "john",
         "surname": "doe",
         "username": "jdoe",
         "email": "jdoe@mail.com",
         "password": "pass1234!",
-        "confirm_password": "pass1234!"
+        "confirm_password": "pass1234!",
+    }
+    res = client.post("/user/", json=data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = data["password"]
+    return new_user
+
+
+@pytest.fixture()
+def test_user2(client: TestClient):
+    data = {
+        "name": "ann",
+        "surname": "toe",
+        "username": "atoe",
+        "email": "atoe@mail.com",
+        "password": "pass1234!",
+        "confirm_password": "pass1234!",
     }
     res = client.post("/user/", json=data)
     assert res.status_code == 201
@@ -73,15 +82,12 @@ def token(test_user):
 
 @pytest.fixture()
 def authorize_client(client, token):
-    client.headers = {
-        **client.headers,
-        "Authorization": f"Bearer {token}"
-    }
+    client.headers = {**client.headers, "Authorization": f"Bearer {token}"}
     return client
 
 
 @pytest.fixture()
-def test_tasks(test_user, session) -> List[DbTask]:
+def test_tasks(test_user, test_user2, session) -> List[DbTask]:
     data = [
         {
             "title": "first task",
@@ -94,13 +100,44 @@ def test_tasks(test_user, session) -> List[DbTask]:
             "description": "Description of the second task",
             "status": "In Progress",
             "priority": "High",
-        }
+        },
     ]
-
-    db_tasks = list(map(
-        lambda tasks: crud_task.create_task(session, request=TaskCreate(**tasks), user_id=test_user["id"]),
-        data
-    ))
+    db_tasks = []
+    for element, user in zip(data, [test_user, test_user2]):
+        print(user["name"])
+        db_tasks.append(
+            crud_task.create(session, request=TaskCreate(**element), user_id=user["id"])
+        )
     session.add_all(db_tasks)
     session.commit()
     return db_tasks
+
+
+@pytest.fixture()
+def test_groups(test_user, test_user2, session):
+    data = [
+        {
+            "name": "group1",
+            "description": "Description of the group1",
+            "admins": [],
+            "members": [],
+            "visibility": True,
+        },
+        {
+            "name": "group2",
+            "description": "Description of the group2",
+            "admins": [],
+            "members": [],
+            "visibility": True,
+        },
+    ]
+    db_groups = []
+    for element, user in zip(data, [test_user, test_user2]):
+        db_groups.append(
+            crud_group.create(session, GroupCreate(**element), user_id=user["id"])
+        )
+    session.add_all(db_groups)
+    session.commit()
+    groups = session.query(DbGroup).all()
+
+    return groups
