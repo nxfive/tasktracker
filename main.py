057817f17dd05943358b4796e154859a864605b7
nxfive <nxfivecom@gmail.com>
2023-10-29 23:18:06 +0100

    feat: Create get_app func for application setup

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from database.setup import Base, engine
 from fastapi import FastAPI
-from routers import user, group, task, label, board, admin
-from auth import authentication
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import get_settings
+from routers import api
+from database.setup import get_engine, Base
 
-app = FastAPI()
 
-app.include_router(authentication.router)
-app.include_router(user.router)
-app.include_router(task.router)
-app.include_router(group.router)
-app.include_router(label.router)
-app.include_router(board.router)
-app.include_router(admin.router)
+def get_app() -> FastAPI:
+    get_settings.cache_clear()
+    settings = get_settings()
+    application = FastAPI(**settings.fastapi_kwargs)
 
-Base.metadata.create_all(engine)
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    application.include_router(api.router)
 
+    if settings.env_state != "test":
+        Base.metadata.create_all(bind=get_engine())
+    return application
+
+
+app = get_app()
