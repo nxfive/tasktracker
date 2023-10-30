423e8c338eaf33a1d0d043fe3c6d11c196ab77ef
nxfive <nxfivecom@gmail.com>
2023-10-30 23:25:35 +0100

    feat: Add exception handlers to task endpoints

--- routers/task.py
+++ routers/task.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from typing import List, Annotated
 from schemas.task import TaskDisplay, TaskUpdate, TaskCreate
 from schemas.user import UserBase
 from sqlalchemy.orm import Session
 from database.setup import get_db
 from crud.task import crud_task
-from auth.oauth2 import get_current_user
+from app.auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix="/tasks",
@@ -13,31 +13,67 @@ router = APIRouter(
 )
 
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=List[TaskDisplay])
-def get_all_tasks(db: Annotated[Session, Depends(get_db)],
-                  current_user: Annotated[UserBase, Depends(get_current_user)]):
-    return crud_task.get_all_tasks(db, user_id=current_user.id)
+@router.get("/", response_model=List[TaskDisplay])
+def get_all_tasks(
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[UserBase, Depends(get_current_user)],
+):
+    return crud_task.get_all(db, user_id=current_user.id)
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-def create_task(request: TaskCreate,
-                db: Annotated[Session, Depends(get_db)],
-                current_user: Annotated[UserBase, Depends(get_current_user)]
-                ):
-    return crud_task.create_task(db, request, user_id=current_user.id)
+@router.get("/{task_id}", response_model=TaskDisplay)
+def get_task_by_id(
+    task_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[UserBase, Depends(get_current_user)],
+):
+    task = crud_task.get_by_id(db, task_id=task_id)
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
+        )
+    if task.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+        )
+    return task
 
 
-@router.patch("/update/{task_id}", status_code=status.HTTP_200_OK)
-def update_task(task_id: int, request: TaskUpdate,
-                db: Annotated[Session, Depends(get_db)],
-                current_user: Annotated[UserBase, Depends(get_current_user)]
-                ):
-    return crud_task.update_task(db, request, task_id=task_id, user_id=current_user.id)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=TaskDisplay)
+def create_task(
+    request: TaskCreate,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[UserBase, Depends(get_current_user)],
+):
+    return crud_task.create(db, request, user_id=current_user.id)
+
+
+@router.patch(
+    "/update/{task_id}", status_code=status.HTTP_200_OK, response_model=TaskDisplay
+)
+def update_task(
+    task_id: int,
+    request: TaskUpdate,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[UserBase, Depends(get_current_user)],
+):
+    task = crud_task.get_by_id(db, task_id=task_id)
+    if not task or task.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
+        )
+    return crud_task.update(db, request, task_id=task.id, user_id=current_user.id)
 
 
 @router.delete("/delete/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(task_id: int,
-                db: Annotated[Session, Depends(get_db)],
-                current_user: Annotated[UserBase, Depends(get_current_user)]
-                ):
-    return crud_task.delete_task(db, task_id=task_id, user_id=current_user.id)
+def delete_task(
+    task_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[UserBase, Depends(get_current_user)],
+):
+    task = crud_task.get_by_id(db, task_id=task_id)
+    if not task or task.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
+        )
+    return crud_task.delete(db, task)
