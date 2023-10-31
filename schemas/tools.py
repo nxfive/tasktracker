63905963fa977ad1534f6d12bbbdc4ce60860874
nxfive <nxfivecom@gmail.com>
2023-10-31 16:03:19 +0100

    feat: Change the exceptions raised in each cases to ValueError

--- schemas/tools.py
+++ schemas/tools.py
@@ -1,22 +1,15 @@
-from exceptions.custom import (
-    PasswordChangeError,
-    PasswordMatchError
-)
-
-
 class Validate:
-
     @staticmethod
     def validate_password_change(old: str, new: str, confirm: str):
         if new and not old:
-            raise PasswordChangeError("Old password must be provided!")
+            raise ValueError("Old password must be provided!")
         if new and not confirm:
-            raise PasswordChangeError("Please confirm the password")
+            raise ValueError("Please confirm the password")
         else:
             if new != confirm:
-                raise PasswordChangeError("Passwords are not the same")
+                raise ValueError("Passwords are not the same")
 
     @staticmethod
     def validate_passwords_match(new: str, confirm: str):
         if new != confirm:
-            raise PasswordMatchError
+            raise ValueError("Passwords do not match")
