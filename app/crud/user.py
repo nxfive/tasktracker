d9ac8ea3556f973c0c00a8c2a2be224adb987543
nxfive <nxfivecom@gmail.com>
2023-11-01 13:21:20 +0100

    feat: Make CrudUser inherit from CrudBase class

--- app/crud/user.py
+++ app/crud/user.py
@@ -2,33 +2,21 @@ from sqlalchemy.orm import Session
 from app.models.user import DbUser
 from app.schemas.user import UserCreate, UserUpdate
 from app.database.hash import Hash
+from app.crud.base import CrudBase
 
 
-class CrudUser:
-
-    def __init__(self):
-        self.model = DbUser
-
-    def get_user_by_username(self, db: Session, *, username: str):
-        return db.query(self.model).filter_by(username=username).first()
-
-    def get_all_users(self, db: Session):
-        return db.query(self.model).all()
-
-    def create_user(self, db: Session, request: UserCreate):
+class CrudUser(CrudBase[DbUser]):
+    def create(self, db: Session, request: UserCreate):
         new_user = self.model(
-            name=request.name,
-            surname=request.surname,
-            username=request.username,
-            email=request.email,
-            password=Hash.bcrypt(request.password)
+            **request.dict(exclude={"password", "confirm_password"}),
+            password=Hash.bcrypt(request.password),
         )
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
         return new_user
 
-    def update_user(self, db: Session, request: UserUpdate, *, user_id: int):
+    def update(self, db: Session, request: UserUpdate, *, user_id: int):
         user = db.query(self.model).filter_by(id=user_id).first()
         if request.new_password and Hash.verify(request.old_password, user.password):
             setattr(user, "password", request.new_password)
@@ -38,11 +26,11 @@ class CrudUser:
         db.refresh(user)
         return user
 
-    def delete_user(self, db: Session, *, user_id: int):
+    def delete(self, db: Session, *, user_id: int):
         user = db.query(self.model).filter_by(id=user_id).first()
         setattr(user, "account", "deactivated")
         db.commit()
         return f"User {user.username} account has been deactivated"
 
 
-crud_user = CrudUser()
+crud_user = CrudUser(DbUser)
