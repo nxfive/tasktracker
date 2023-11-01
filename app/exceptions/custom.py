2cef0c740da830a98265800135298429c457cae8
nxfive <nxfivecom@gmail.com>
2023-11-01 22:58:01 +0100

    feat: Create exceptions EntityNotExist and PermissionDenied
    Both inherits from HTTPException.

--- app/exceptions/custom.py
+++ app/exceptions/custom.py
@@ -3,12 +3,25 @@ from fastapi import HTTPException
 
 class PasswordChangeError(HTTPException):
     def __init__(self, detail: str):
-        status_code = 400
-        super().__init__(status_code=status_code, detail=detail)
+        super().__init__(status_code=400, detail=detail)
 
 
 class PasswordMatchError(HTTPException):
     def __init__(self):
-        status_code = 400
-        detail = "Passwords do not match. Please make sure the new password and confirmation password are identical."
-        super().__init__(status_code=status_code, detail=detail)
+        super().__init__(
+            status_code=400,
+            detail="Passwords not match",
+        )
+
+
+class EntityNotExist(HTTPException):
+    def __init__(self, entity: str):
+        super().__init__(status_code=404, detail=f"{entity.title()} not found")
+
+
+class PermissionDenied(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=403,
+            detail="Access denied. No permission to perform this operation.",
+        )
