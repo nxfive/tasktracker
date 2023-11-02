e0dcac107d785dbeba5427681352a09bfca066cd
nxfive <nxfivecom@gmail.com>
2023-11-02 17:07:08 +0100

    fix: Change async func get_current_user to regualar func

--- app/auth/oauth2.py
+++ app/auth/oauth2.py
@@ -28,7 +28,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(
+def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     db: Annotated[Session, Depends(get_db)],
 ):
