d3c3a59d5d362c2df00e41b889756e0a911b2e7a
nxfive <nxfivecom@gmail.com>
2023-11-02 17:12:51 +0100

    feat: Del __init__, get_all_admins and assign funcs
    Func __init__ and get_all_admins are declared in parent class.

--- app/crud/admin.py
+++ app/crud/admin.py
@@ -2,23 +2,13 @@ from sqlalchemy.orm import Session
 from app.models.admin import DbAdmin
 from app.schemas.admin import AdminCreate, AdminUpdate
 from app.database.hash import Hash
-from app.crud.user import CrudUser
-from typing import List, Dict
+from app.crud.base import CrudBase
 
 
-class CrudAdmin(CrudUser):
-    def __init__(self):
-        super().__init__()
-        self.admin_model = DbAdmin
-
-    def get_all_admins(self, db: Session) -> List[DbAdmin]:
-        return db.query(self.admin_model).all()
-
+class CrudAdmin(CrudBase[DbAdmin]):
     def create_admin(self, db: Session, request: AdminCreate) -> DbAdmin:
-        new_admin = self.admin_model(
-            name=request.name,
-            surname=request.surname,
-            email=request.email,
+        new_admin = self.model(
+            **request.dict(exclude={"password", "confirm_password"}),
             password=Hash.bcrypt(request.password),
         )
         db.add(new_admin)
@@ -28,8 +18,8 @@ class CrudAdmin(CrudUser):
 
     def update_admin(
         self, db: Session, request: AdminUpdate, *, admin_id: int
-    ) -> DbAdmin:
-        admin = db.query(self.admin_model).filter_by(id=admin_id).first()
+    ) -> DbAdmin | None:
+        admin = db.query(self.model).filter_by(id=admin_id).first()
         if request.new_password and Hash.verify(request.old_password, admin.password):
             setattr(admin, "password", request.new_password)
         if request.email:
@@ -38,10 +28,5 @@ class CrudAdmin(CrudUser):
         db.refresh(admin)
         return admin
 
-    def assign(self, db: Session, user_id: int) -> Dict[str]:
-        user = db.query(self.user_model).filter_by(id=user_id).first()
-        setattr(user, "role", "admin")
-        return {"message": f"User {user.username} role has changed to admin"}
 
-
-crud_admin = CrudAdmin()
+crud_admin = CrudAdmin(DbAdmin)
