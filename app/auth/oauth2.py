e4f229d339793d6d16ed09d92e557a4395dfb1ad
nxfive <nxfivecom@gmail.com>
2023-11-03 23:03:54 +0100

    feat: Add ExpiredSignatureError exception in get_current_user
    It informs user that the token has expired.

--- app/auth/oauth2.py
+++ app/auth/oauth2.py
@@ -1,21 +1,23 @@
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
 from datetime import datetime, timedelta
-from app.database.setup import get_db
+from jose import jwt, JWTError, ExpiredSignatureError
+from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.crud.user import crud_user
-from app.schemas.user import UserCheck
 from typing import Annotated
+
+from app.database.setup import get_db
+from app.crud.user import crud_user
 from app.core.config import get_settings
 from app.exceptions.custom import EntityNotExist
+from app.models.user import DbUser
+from app.schemas.user import UserCheck
 
 settings = get_settings()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -31,7 +33,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     db: Annotated[Session, Depends(get_db)],
-):
+) -> DbUser | None:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -45,8 +47,14 @@ def get_current_user(
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-    except JWTError:
-        raise credentials_exception
+    except ExpiredSignatureError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from e
+    except JWTError as e:
+        raise credentials_exception from e
 
     user = crud_user.get_by_attr(db, attr_name="username", value=username)
     if user is None:
