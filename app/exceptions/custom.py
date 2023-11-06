73a64d3220c752c65a4e7a0acece3174115e3ead
nxfive <nxfivecom@gmail.com>
2023-11-06 23:26:37 +0100

    fix: Delete PasswordChangeError and PasswordMatchError

--- app/exceptions/custom.py
+++ app/exceptions/custom.py
@@ -1,19 +1,6 @@
 from fastapi import HTTPException
 
 
-class PasswordChangeError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=400, detail=detail)
-
-
-class PasswordMatchError(HTTPException):
-    def __init__(self):
-        super().__init__(
-            status_code=400,
-            detail="Passwords not match",
-        )
-
-
 class EntityNotExist(HTTPException):
     def __init__(self, entity: str):
         super().__init__(status_code=404, detail=f"{entity.title()} not found")
